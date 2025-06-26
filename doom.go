a0423bf25ea0a238cb29afdec41cb1290ff6e3e7
Andre Renaud <andre@ignavus.net>
2025-06-26 21:07:25 +1200

    dead code check

--- doom.go
+++ doom.go
@@ -9441,6 +9441,9 @@ func HUlib_delCharFromTextLine(t *hu_textline_t) boolean {
 	}
 }
 
+func wibble() {
+
+}
 func HUlib_drawTextLine(l *hu_textline_t, drawcursor boolean) {
 	var c uint8
 	var i, w, x int32
