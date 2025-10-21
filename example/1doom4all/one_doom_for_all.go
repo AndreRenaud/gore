236435af181200d6861250028007877ee9e9b2e2
Andre Renaud <andre@ignavus.net>
2025-10-22 10:41:51 +1300

    Numeric key support

--- example/1doom4all/one_doom_for_all.go
+++ example/1doom4all/one_doom_for_all.go
@@ -57,7 +57,28 @@ func mapBrowserKeyToDoom(key int) uint8 {
 		return 'y'
 	case 78: // N
 		return 'n'
+	case '0':
+		return '0'
+	case '1':
+		return '1'
+	case '2':
+		return '2'
+	case '3':
+		return '3'
+	case '4':
+		return '4'
+	case '5':
+		return '5'
+	case '6':
+		return '6'
+	case '7':
+		return '7'
+	case '8':
+		return '8'
+	case '9':
+		return '9'
 	default:
+		log.Printf("Unsupported key code: %d", key)
 		return 0
 	}
 }
